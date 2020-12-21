f608d846bd546c922727c19186eede357510e6ab
YuunaTsukisagi <emilie.yuu@gmail.com>
2020-12-21 14:28:26 +0100

    Add products list

---
+++ app.js
@@ -0,0 +1,21 @@
+let productsContainer = document.getElementById("products");
+
+fetch("http://localhost:3000/api/teddies")
+.then(response => response.json())
+.then(teddies => {
+  for (let i = 0; i < teddies.length; i++) {
+    //Get all teddies properties into variables
+    const {name, price, imageUrl} = teddies[i];
+
+    //Construct html tags with variables
+    const nameTag = '<h2 class="products__name">' + name + '</h2>';
+    const imgTag = '<img src="' + imageUrl + '" class="products__img" alt="' + name + '">';
+    const priceTag = '<span class="products__price">' + price + '</span>';
+
+    //Create container div
+    let element = document.createElement("div");
+    element.classList.add("products__product");
+    element.innerHTML = nameTag + imgTag + priceTag;
+    productsContainer.appendChild(element);
+  }
+});
