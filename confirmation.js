a73766dfa2e225a39d6b6428587186c559aa7245
YuunaTsukisagi <emilie.yuu@gmail.com>
2021-01-19 14:51:16 +0100

    Add price to confirmation page and improve confirmation page design

--- confirmation.js
+++ confirmation.js
@@ -7,16 +7,14 @@ const ORDER_PRICE = document.getElementById("order-price");
 const ORDER_ID = document.getElementById("order-id");
 ORDER_FIRSTNAME.textContent = firstName;
 ORDER_ID.textContent = ORDER_CONFIRMATION.orderId;
-ORDER_PRICE.textContent = "€";
+ORDER_PRICE.textContent = localStorage.getItem("cart_total") + "€";
 
 const CUSTOMER_DATA = document.getElementById("customer");
 CUSTOMER_DATA.innerHTML = `
-  <ul>
-    <li>Nom et prénom : ${lastName} ${firstName}</li>
-    <li>Rue et n° de rue : ${address}</li>
-    <li>Ville : ${city}</li>
-    <li>E-mail : ${email}</li>
-  </ul>
+  <li><span class="strong">Nom et prénom :</span> ${lastName} ${firstName}</li>
+  <li><span class="strong">Rue et n° de rue :</span> ${address}</li>
+  <li><span class="strong">Ville :</span> ${city}</li>
+  <li><span class="strong">E-mail :</span> ${email}</li>
 `;
 
 localStorage.clear();
